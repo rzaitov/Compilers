2e02f051479c392e56022da99359a722ec1c7ada
Rustam Zaitov <rzaitov@Rustams-Mac-mini.local>
2014-04-04 02:52:33 +0400

    Реализовал простой транслятор из инфиксных выражений в постфиксные методом рекурсивного спуска

---
+++ InfixToPostfix/InfixToPostfix/Parser.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+
+namespace InfixToPostfix
+{
+	public class Parser
+	{
+		private readonly string _input;
+
+		private StringBuilder _sb;
+		public string Result
+		{
+			get { return _sb.ToString(); }
+		}
+
+		private int _position;
+		private char CurrentToken
+		{
+			get
+			{
+				return _position < _input.Length ? _input[_position] : (char)0;
+			}
+		}
+
+		public Parser(string input)
+		{
+			_input = input;
+			_position = 0;
+
+			_sb = new StringBuilder();
+		}
+
+		public void Expr()
+		{
+			Term(); Rest();
+		}
+
+		public void Rest()
+		{
+			switch(CurrentToken)
+			{
+				case '+':
+					Match('+');
+					Term();
+					_sb.Append('+');
+					Rest();
+					break;
+
+				case '-':
+					Match('-');
+					Term();
+					_sb.Append('-');
+					Rest();
+					break;
+
+				default:
+					return;
+					break;
+			}
+		}
+
+		public void Term()
+		{
+			if(Char.IsDigit(CurrentToken))
+			{
+				char tmp = CurrentToken;
+				Match(CurrentToken);
+				_sb.Append(tmp);
+			}
+			else
+			{
+				throw new InvalidOperationException();
+			}
+		}
+
+		public void Match(char token)
+		{
+			if(CurrentToken == token)
+				_position++;
+			else
+				throw new InvalidOperationException();
+		}
+	}
+}
+
---
+++ InfixToPostfix/InfixToPostfix/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InfixToPostfix
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Parser p = new Parser("9-5+2");
+			p.Expr();
+
+			Console.WriteLine(p.Result);
+		}
+	}
+}
